2972de82d855ae53691136c42a96a02b4782846b
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-04-27 02:47:47 +0500

    night out section completed

--- assets/scripts/owlcarousel/main.js
+++ assets/scripts/owlcarousel/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel(
+      {
+        // loop:false,
+        margin:0,
+        nav:false,
+        responsiveClass:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1.3,
+                loop:true,
+                center:true
+            },
+            960:{
+                items:4,
+                loop:false,
+            },
+        }
+    }
+      
+    );
   });
