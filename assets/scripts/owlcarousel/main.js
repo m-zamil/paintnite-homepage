c10bcb19b158cc04ffefdef513a2d23123be72c3
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-06-24 23:13:11 +0500

    instagram feed updated

--- assets/scripts/owlcarousel/main.js
+++ assets/scripts/owlcarousel/main.js
@@ -24,4 +24,23 @@ $(document).ready(function () {
       $(this).removeClass("hover");
     }
   );
+
+  var imageCount;
+  if (screen.width <= 960) {
+    imageCount = 4;
+  } else imageCount = 9;
+
+  var feed = new Instafeed({
+    get: "tagged",
+    tagName: "paintnitedotcom",
+    target: "instafeed-container",
+    template: '<img title="{{caption}}" src="{{image}}" />',
+    limit: imageCount,
+    resolution: "high_resolution",
+    accessToken: "IGQVJVdkh5NXRpS2NMY1lBVE83Y3FXX1pfQnhkd2ZADQjRPRC1BMDhodDI3b1NJZAlBMdnFIeldTWWk2S3gtUmdEdm1idWN2WlluTGNENFI5aUp2V2RmZAEVSaXdmVUZAFcG5fMzVWZAW5zUU5RUUtBYW40RAZDZD",
+    filter: function (image) {
+      return image.model.caption.includes("#paintnitedotcom");
+    },
+  });
+  feed.run();
 });
