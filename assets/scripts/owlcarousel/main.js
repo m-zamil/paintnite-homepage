ecefd5e360004a535fd7dcc20421d88b865ec1ef
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-07-28 11:15:13 +0500

    script loading issue instagram

--- assets/scripts/owlcarousel/main.js
+++ assets/scripts/owlcarousel/main.js
@@ -24,23 +24,22 @@ $(document).ready(function () {
       $(this).removeClass("hover");
     }
   );
-});
 
-var imageCount;
-if (screen.width <= 960) {
-  imageCount = 4;
-} else imageCount = 9;
+  var imageCount;
+  if (screen.width <= 960) {
+    imageCount = 4;
+  } else imageCount = 9;
 
-var feed = new Instafeed({
-  get: "tagged",
-  tagName: "paintnitedotcom",
-  target: "instafeed-container",
-  template: '<img title="{{caption}}" src="{{image}}" />',
-  limit: imageCount,
-  resolution: "high_resolution",
-  accessToken: "IGQVJVVmhzVzlzZAGJYanNyTV9fbkVqQ1I2MzdhU2VsU3BQTjFJSVNFM0ltSXNuYUk0eUowWVI0NmVDdW1WWjA3Wi1selRxZAFJNWE9lOHNDZAzhSM19iaV84ejV5SFlLRVUzR2J4dVR4SERUdlJQNmhrWQZDZD",
-  error: function () {
-    document.getElementById("instafeed-container").innerHTML = `
+  var feed = new Instafeed({
+    get: "tagged",
+    tagName: "paintnitedotcom",
+    target: "instafeed-container",
+    template: '<img title="{{caption}}" src="{{image}}" />',
+    limit: imageCount,
+    resolution: "high_resolution",
+    accessToken: "IGQVJVVmhzVzlzZAGJYanNyTV9fbkVqQ1I2MzdhU2VsU3BQTjFJSVNFM0ltSXNuYUk0eUowWVI0NmVDdW1WWjA3Wi1selRxZAFJNWE9lOHNDZAzhSM19iaV84ejV5SFlLRVUzR2J4dVR4SERUdlJQNmhrWQZDZD",
+    error: function () {
+      document.getElementById("instafeed-container").innerHTML = `
       <img src="https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/social-1.jpg" alt="" />
       <img src="https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/social-2.jpg" alt="" />
       <img src="https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/social-4.jpg" alt="" />
@@ -50,9 +49,10 @@ var feed = new Instafeed({
       <img src="https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/social-7.jpg" alt="" />
       <img src="https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/social-8.jpg" alt="" />
       <img src="https://s3fs.paintnite.com/fresca-web-html/paintnite/landing-page/assets/images/social-9.jpg" alt="" />`;
-  },
-  filter: function (image) {
-    return image.model.caption.includes("#paintnitedotcom");
-  },
+    },
+    filter: function (image) {
+      return image.model.caption.includes("#paintnitedotcom");
+    },
+  });
+  feed.run();
 });
-feed.run();
